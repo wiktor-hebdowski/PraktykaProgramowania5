ff7ac9789032c36c1724067a54370da32c99ded9
Wiktor Hebdowski <275964@student.pwr.edu.pl>
2026-05-28 17:48:10 +0200

    Applying calculator, tests and actions.

---
+++ utils.py
@@ -0,0 +1,14 @@
+#Calculator
+
+
+def add (a: int , b: int) -> int :
+    return a + b
+
+def subtract (a: int , b: int) -> int :
+    return a - b
+
+def multiply (a: int , b: int) -> int :
+    return a * b
+
+def divide ( a: int , b : int ) -> float :
+    return a / b
