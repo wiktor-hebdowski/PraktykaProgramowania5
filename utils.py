f082833778713f623d6af24daea2c3691e326e97
Wiktor Hebdowski <275964@student.pwr.edu.pl>
2026-05-28 18:01:56 +0200

    Reformatted utils.py

--- utils.py
+++ utils.py
@@ -1,21 +1,22 @@
 # Calculator
-'''Calculator'''
+"""Calculator"""
+
 
 def add(a: int, b: int) -> int:
-    '''sums two integers'''
+    """sums two integers"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
-    '''subtracts two integers'''
+    """subtracts two integers"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
-    '''multiplies two integers'''
+    """multiplies two integers"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
-    '''divides two integers'''
+    """divides two integers"""
     return a / b
