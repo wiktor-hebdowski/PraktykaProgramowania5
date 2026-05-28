e0735d0871df699a044a5b00cfc4e5b298a07b04
Wiktor Hebdowski <275964@student.pwr.edu.pl>
2026-05-28 17:58:16 +0200

    Fixed job problems

--- utils.py
+++ utils.py
@@ -1,14 +1,21 @@
-#Calculator
+# Calculator
 
 
-def add (a: int , b: int) -> int :
+def add(a: int, b: int) -> int:
+    '''sums two integers'''
     return a + b
 
-def subtract (a: int , b: int) -> int :
+
+def subtract(a: int, b: int) -> int:
+    '''subtracts two integers'''
     return a - b
 
-def multiply (a: int , b: int) -> int :
+
+def multiply(a: int, b: int) -> int:
+    '''multiplies two integers'''
     return a * b
 
-def divide ( a: int , b : int ) -> float :
+
+def divide(a: int, b: int) -> float:
+    '''divides two integers'''
     return a / b
